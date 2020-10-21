6dcb26cf1e631fb63c5235253a4d35622ae4f549
Louis Bönsch <louisboensch@live.de>
2020-10-21 11:05:38 +0200

    :art::bug: reformatted code and fixed some issues

--- gamescript.js
+++ gamescript.js
@@ -1,28 +1,41 @@
-
 const character = document.getElementById("character");
 const block = document.getElementById("block");
 const score = document.getElementById("scoreSpan");
 let counter = 0;
 
+window.addEventListener('keydown', (evt) => {
+    if (evt.code === "Space") {
+        jump();
+    }
+});
+
 function jump() {
-    if (character.classList == "animate") { return }
-    character.classList.add("animate");
-    setTimeout(function () {
-        character.classList.remove("animate");
-    }, 300);
+  if (character.classList.contains("animate")) {
+    return;
+  }
+  character.classList.add("animate");
+  setTimeout(function () {
+    character.classList.remove("animate");
+  }, 300);
 }
 
-
 const checkDead = setInterval(function () {
-    const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if (blockLeft < 170 && blockLeft > 130 && characterTop >= 350) {
-        block.style.animation = "none";
-        alert("Game Over. score: " + Math.floor(counter / 92));
-        counter = 0;
-        block.style.animation = "block 1s infinite linear";
-    } else {
-        ++counter;
-        score.textContent = Math.floor(counter / 92);
-    }
+  const characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  const blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  if (blockLeft < 170 && blockLeft > 130 && characterTop >= 350) {
+    block.classList.remove("animate");
+    alert(`Game Over. score: ${counter}`);
+    counter = 0;
+    block.classList.add("animate");
+  }
+
+  if (blockLeft == 0) {
+    ++counter;
+  }
+
+  score.textContent = counter;
 }, 10);
